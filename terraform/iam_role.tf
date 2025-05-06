489afc008fcd070cc63995b2acce867a8856606c
phu <phulam11031996@gmail.com>
2025-05-06 13:52:37 -0700

    organize iam role

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -16,18 +16,23 @@ resource "aws_iam_role" "ssm_role" {
   })
 }
 
-# Attach SSM Managed Policy to Role
-resource "aws_iam_role_policy_attachment" "ssm_policy" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+# SSM + S3 + SQS Role for EC2
+resource "aws_iam_role" "ssm_s3_read_sqs_role" {
+  name = "EC2SSMAndS3AndSQSRole"
 
-# Instance Profile for EC2 with SSM Role
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "EC2SSMProfile"
-  role = aws_iam_role.ssm_role.name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
-
 
 # SSM + S3 Role for EC2
 resource "aws_iam_role" "ssm_s3_publish_sns_role" {
@@ -45,13 +50,6 @@ resource "aws_iam_role" "ssm_s3_publish_sns_role" {
       }
     ]
   })
-}
-
-
-# Attach SSM Managed Policy to SSM+S3 Role
-resource "aws_iam_role_policy_attachment" "ssm_s3_ssm_policy" {
-  role       = aws_iam_role.ssm_s3_publish_sns_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 # Custom Inline Policy for S3 Access
@@ -121,28 +119,28 @@ resource "aws_iam_role_policy" "ssm_s3_read_sqs_sqs_policy" {
   })
 }
 
-# SSM + S3 + SQS Role for EC2
-resource "aws_iam_role" "ssm_s3_read_sqs_role" {
-  name = "EC2SSMAndS3AndSQSRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
 # Attach SSM Managed Policy to Role
 resource "aws_iam_role_policy_attachment" "ssm_s3_read_sqs_ssm_policy" {
   role       = aws_iam_role.ssm_s3_read_sqs_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Attach SSM Managed Policy to Role
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Attach SSM Managed Policy to SSM+S3 Role
+resource "aws_iam_role_policy_attachment" "ssm_s3_ssm_policy" {
+  role       = aws_iam_role.ssm_s3_publish_sns_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Instance Profile for EC2 with SSM Role
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2SSMProfile"
+  role = aws_iam_role.ssm_role.name
 }
 
 # Instance Profile for EC2 with SSM, S3, and SQS Role
@@ -150,9 +148,6 @@ resource "aws_iam_instance_profile" "ssm_s3_read_sqs_profile" {
   name = "EC2SSMS3ReadSQSProfile"
   role = aws_iam_role.ssm_s3_read_sqs_role.name
 }
-
-
-
 
 # Instance Profile for EC2 with SSM+S3 Role
 resource "aws_iam_instance_profile" "ssm_s3_publish_sns_profile" {
